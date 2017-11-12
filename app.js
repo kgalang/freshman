13d68bd553dfb6e922413e86ff1e7468ff861ce6
collinsnji <collins.geek@gmail.com>
2017-11-12 09:13:07 -0500

    Add intro page

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var users = require('./routes/clubs');
 var login = require('./routes/login');
 
 var app = express();
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/home', index);
-app.use('/', index);
+app.use('/', login);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
