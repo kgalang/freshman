c6966b20f0925a1dc4d7a8dbc7b41af03fd52075
Rafael Medina <rafaelmedina@outlook.com>
2017-11-11 23:42:40 -0500

    add layout links

--- config.rb
+++ config.rb
@@ -2,4 +2,4 @@ css_dir = "public/stylesheets"
 sass_dir = "public/sass"
 output_style = :nested
 relative_assets = true
-line_comments = falsehttps://github.com/collinsnji/freshman
+line_comments = false
