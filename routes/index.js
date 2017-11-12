cd50b882c6bf0cd88bf891b9565857a79d8c4aec
collinsnji <collins.geek@gmail.com>
2017-11-11 21:15:49 -0500

    Making shit work

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 var app = express();
-var path = require('path');
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-var port = process.env.PORT || 3000;
-var fs = require('fs');
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
